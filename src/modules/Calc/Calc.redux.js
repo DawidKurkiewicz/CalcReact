54caebebab6537786ba0459e3bd2c2b0434a9147
Kurkiewicz <kurkiewd@emea.roche.com>
2021-06-29 11:33:38 +0200

    redux v2

--- src/modules/Calc/Calc.redux.js
+++ src/modules/Calc/Calc.redux.js
@@ -3,11 +3,11 @@ export const CALC_SET_IS_LOADING = 'SET CALC IS LOADING'
 export const CALC_ADD_RESULT = 'ADD CALC RESULT'
 export const CALC_REMOVE_RESULT = 'REMOVE CALC RESULT'
 //1
-// export const CALC_ADD_VALUE = 'ADD CALC VALUE'
-// export const CALC_CLEAR = 'CLEAR INPUT'
-// export const CALC_COUNT = 'COUNT'
-// export const CALC_EVAL = 'EVAL'
-// export const CALC_DOT = 'DOT'
+export const CALC_ADD_VALUE = 'ADD CALC VALUE'
+export const CALC_CLEAR = 'CLEAR INPUT'
+export const CALC_COUNT = 'COUNT'
+export const CALC_EVAL = 'EVAL'
+export const CALC_DOT = 'DOT'
 
 export const setIsLoading = () => ({
     type: CALC_SET_IS_LOADING,
@@ -22,32 +22,32 @@ export const removeResult = (val) => ({
     value: val
 })
 //2
-// export const addRValue = (val) => ({
-//     type: CALC_ADD_VALUE,
-//     value: val
-// })
-// export const clearRInput = () => ({
-//     type: CALC_CLEAR
-// })
-// export const countR = (val) => ({
-//     type: CALC_COUNT,
-//     value: val
-// })
-// export const evaluateR = () => ({
-//     type: CALC_EVAL
-// })
-// export const addDotR = () => ({
-//     type: CALC_DOT
-// })
+export const addRValue = (val) => ({
+    type: CALC_ADD_VALUE,
+    value: val
+})
+export const clearRInput = () => ({
+    type: CALC_CLEAR
+})
+export const countR = (val) => ({
+    type: CALC_COUNT,
+    value: val
+})
+export const evaluateR = () => ({
+    type: CALC_EVAL
+})
+export const addDotR = () => ({
+    type: CALC_DOT
+})
 
 export const initialState = {
     isLoading: false,
     results: [],
     //3
-    // inputValue: ''
+    inputValue: ''
 }
 //4
-// const regex = new RegExp(/[\W_]/);
+const regex = new RegExp(/[\W_]/);
 
 export const calcReducer = (store = initialState, action) => {
     switch (action.type) {
@@ -76,68 +76,68 @@ export const calcReducer = (store = initialState, action) => {
 
             }
             //5
-        // case CALC_ADD_VALUE:
-        //     if (store.inputValue === "0" || (action.value === "0" && store.inputValue === "")) {
-        //         store.inputValue = "0."
-        //     } else {
-        //         store.inputValue = store.inputValue + action.value;
-        //     }
-        //     console.log(action)
-        //     return {
-        //         ...store,
-        //         inputValue: store.inputValue
-        //     }
+        case CALC_ADD_VALUE:
+            if (store.inputValue === "0" || (action.value === "0" && store.inputValue === "")) {
+                store.inputValue = "0."
+            } else {
+                store.inputValue = store.inputValue + action.value;
+            }
+            console.log(action)
+            return {
+                ...store,
+                inputValue: store.inputValue
+            }
 
-        // case CALC_CLEAR:
-        //     console.log(store)
-        //     store.inputValue = ''
-        //     return {
-        //         ...store,
-        //         inputValue: store.inputValue
-        //     }
-        // case CALC_COUNT:
-        //     console.log(action.value)
-        //     if (store.inputValue.toString() !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
-        //         store.inputValue = store.inputValue + action.value
-        //     } else if (store.inputValue !== "") {
-        //         store.inputValue = store.inputValue.toString().slice(0, -1) + action.value
-        //     }
-        //     return {
-        //         ...store,
-        //         inputValue: store.inputValue
-        //     }
-        // case CALC_EVAL:
-        //     if (store.inputValue !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
-        //         // eslint-disable-next-line
-        //         const result = eval(store.inputValue)
-        //         store.results = [...store.results, result]
-        //         console.log(result)
-        //         store.inputValue = result
-        //     } else if (store.inputValue !== "") {
-        //         // eslint-disable-next-line
-        //         const result = eval(store.inputValue.toString().slice(0, -1))
-        //         store.results = [...store.results, result]
-        //         store.inputValue = result
-        //     }
-        //     return {
-        //         ...store,
-        //         results: store.results,
-        //         inputValue: store.inputValue
-        //     }
-        // case CALC_DOT:
-        //     if (store.inputValue === "" || (store.inputValue === "0" && store.inputValue.toString().slice(-1).match(regex) == null)) {
-        //         store.inputValue = "0."
-        //     } else if (store.inputValue.toString().slice(-1).match(regex)) {
-        //         // eslint-disable-next-line
-        //         store.inputValue = store.inputValue
-        //     }
-        //     else {
-        //         store.inputValue = store.inputValue + "."
-        //     }
-        //     return {
-        //         ...store,
-        //         inputValue: store.inputValue
-        //     }
+        case CALC_CLEAR:
+            console.log(store)
+            store.inputValue = ''
+            return {
+                ...store,
+                inputValue: store.inputValue
+            }
+        case CALC_COUNT:
+            console.log(action.value)
+            if (store.inputValue.toString() !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
+                store.inputValue = store.inputValue + action.value
+            } else if (store.inputValue !== "") {
+                store.inputValue = store.inputValue.toString().slice(0, -1) + action.value
+            }
+            return {
+                ...store,
+                inputValue: store.inputValue
+            }
+        case CALC_EVAL:
+            if (store.inputValue !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
+                // eslint-disable-next-line
+                const result = eval(store.inputValue)
+                store.results = [...store.results, result]
+                console.log(result)
+                store.inputValue = result
+            } else if (store.inputValue !== "") {
+                // eslint-disable-next-line
+                const result = eval(store.inputValue.toString().slice(0, -1))
+                store.results = [...store.results, result]
+                store.inputValue = result
+            }
+            return {
+                ...store,
+                results: store.results,
+                inputValue: store.inputValue
+            }
+        case CALC_DOT:
+            if (store.inputValue === "" || (store.inputValue === "0" && store.inputValue.toString().slice(-1).match(regex) == null)) {
+                store.inputValue = "0."
+            } else if (store.inputValue.toString().slice(-1).match(regex)) {
+                // eslint-disable-next-line
+                store.inputValue = store.inputValue
+            }
+            else {
+                store.inputValue = store.inputValue + "."
+            }
+            return {
+                ...store,
+                inputValue: store.inputValue
+            }
         default:
             return store;
     }
--- src/routes/Calc/Calc.js
+++ src/routes/Calc/Calc.js
@@ -1,6 +1,6 @@
 //1
-import React, { useState } from 'react'
-// import React from 'react';
+// import React, { useState } from 'react'
+import React from 'react';
 
 import { Button, Result, ClearButton } from "components";
 import style from "./Calc.module.css";
@@ -8,103 +8,63 @@ import style from "./Calc.module.css";
 import { Link } from 'react-router-dom';
 
 //2
-import { addResult, setIsLoading} from 'modules/Calc/Calc.redux';
-// import {setIsLoading,addRValue, clearRInput, countR, evaluateR, addDotR} from 'modules/Calc/Calc.redux';
+// import { addResult, setIsLoading} from 'modules/Calc/Calc.redux';
+import {setIsLoading,addRValue, clearRInput, countR, evaluateR, addDotR} from 'modules/Calc/Calc.redux';
 
 import { connect } from 'react-redux';
 
 //3
-  const CalcComponent = ({addResult,setIsLoading}) => { 
-const [inputValue, setInputValue] = useState('');
-  const regex = new RegExp(/[\W_]/);
+//   const CalcComponent = ({addResult,setIsLoading}) => { 
+// const [inputValue, setInputValue] = useState('');
+//   const regex = new RegExp(/[\W_]/);
 
-  // const { addResult } = useMainContext();
+//   // const { addResult } = useMainContext();
 
-  const addValue = val => {
-    console.log(val)
-    if (inputValue === "0" || (val === "0" && inputValue === "")) {
-      setInputValue("0.")
-    } else {
-      setInputValue(inputValue + val);
-    }
-  }
+//   const addValue = val => {
+//     console.log(val)
+//     if (inputValue === "0" || (val === "0" && inputValue === "")) {
+//       setInputValue("0.")
+//     } else {
+//       setInputValue(inputValue + val);
+//     }
+//   }
 
-  const clearInput = () => {
-    setInputValue("");
-  }
+//   const clearInput = () => {
+//     setInputValue("");
+//   }
 
-  const count = val => {
-    if (inputValue.toString() !== "" && inputValue.toString().slice(-1).match(regex) == null) {
-      setInputValue(inputValue + val);
-    } else if (inputValue !== "") {
-      setInputValue(inputValue.toString().slice(0, -1) + val);
-    }
-  }
+//   const count = val => {
+//     if (inputValue.toString() !== "" && inputValue.toString().slice(-1).match(regex) == null) {
+//       setInputValue(inputValue + val);
+//     } else if (inputValue !== "") {
+//       setInputValue(inputValue.toString().slice(0, -1) + val);
+//     }
+//   }
 
-  const evaluate = () => {
-    if (inputValue !== "" && inputValue.toString().slice(-1).match(regex) == null) {
-      // eslint-disable-next-line
-     const result = eval(inputValue)
-      addResult(result)
-      setInputValue(result)
-    } else if (inputValue !== "") {
-      // eslint-disable-next-line
-      const result = eval(inputValue.toString().slice(0, -1))
-      addResult(result)
-      setInputValue(result)
-    }
-  }
+//   const evaluate = () => {
+//     if (inputValue !== "" && inputValue.toString().slice(-1).match(regex) == null) {
+//       // eslint-disable-next-line
+//      const result = eval(inputValue)
+//       addResult(result)
+//       setInputValue(result)
+//     } else if (inputValue !== "") {
+//       // eslint-disable-next-line
+//       const result = eval(inputValue.toString().slice(0, -1))
+//       addResult(result)
+//       setInputValue(result)
+//     }
+//   }
 
-  const addDot = () => {
-    if (inputValue === "" || (inputValue === "0" && inputValue.toString().slice(-1).match(regex) == null)) {
-      setInputValue("0.")
-    } else if (inputValue.toString().slice(-1).match(regex)) {
-      setInputValue(inputValue)
-    }
-    else {
-      setInputValue(inputValue + ".")
-    }
-  }
-  return (
-    <div className={style.calcContainer}>
-            <Link to="/Second">Results</Link>
-            <button onClick={() => setIsLoading()}>set calc is loading</button>
-            <Link to="/Chat">Chat</Link>
-
-      <div className={style.row}>
-        <Result inputValue={inputValue} />
-        <ClearButton handleClear={clearInput}>Clear</ClearButton>
-      </div>
-      <div className={style.numberContainer}>
-        <div className={style.row}>
-
-          <Button handleClick={addValue}>7</Button>
-          <Button handleClick={addValue}>8</Button>
-          <Button handleClick={addValue}>9</Button>
-          <Button handleClick={count}>/</Button>
-        </div>
-        <div className={style.row}>
-          <Button handleClick={addValue}>4</Button>
-          <Button handleClick={addValue}>5</Button>
-          <Button handleClick={addValue}>6</Button>
-          <Button handleClick={count}>*</Button>
-        </div>
-        <div className={style.row}>
-          <Button handleClick={addValue}>1</Button>
-          <Button handleClick={addValue}>2</Button>
-          <Button handleClick={addValue}>3</Button>
-          <Button handleClick={count}>+</Button>
-        </div>
-        <div className={style.row}>
-          <Button handleClick={addDot}>.</Button>
-          <Button handleClick={addValue}>0</Button>
-          <Button handleClick={evaluate}>=</Button>
-          <Button handleClick={count}>-</Button>
-        </div>
-      </div>
-    </div>
-  );
-// const CalcComponent = ({inputValue,setIsLoading ,addRValue, clearRInput, countR, evaluateR, addDotR }) => {
+//   const addDot = () => {
+//     if (inputValue === "" || (inputValue === "0" && inputValue.toString().slice(-1).match(regex) == null)) {
+//       setInputValue("0.")
+//     } else if (inputValue.toString().slice(-1).match(regex)) {
+//       setInputValue(inputValue)
+//     }
+//     else {
+//       setInputValue(inputValue + ".")
+//     }
+//   }
 //   return (
 //     <div className={style.calcContainer}>
 //             <Link to="/Second">Results</Link>
@@ -113,54 +73,94 @@ const [inputValue, setInputValue] = useState('');
 
 //       <div className={style.row}>
 //         <Result inputValue={inputValue} />
-//         <ClearButton handleClear={clearRInput}>Clear</ClearButton>
+//         <ClearButton handleClear={clearInput}>Clear</ClearButton>
 //       </div>
 //       <div className={style.numberContainer}>
 //         <div className={style.row}>
 
-//           <Button handleClick={addRValue}>7</Button>
-//           <Button handleClick={addRValue}>8</Button>
-//           <Button handleClick={addRValue}>9</Button>
-//           <Button handleClick={countR}>/</Button>
+//           <Button handleClick={addValue}>7</Button>
+//           <Button handleClick={addValue}>8</Button>
+//           <Button handleClick={addValue}>9</Button>
+//           <Button handleClick={count}>/</Button>
 //         </div>
 //         <div className={style.row}>
-//           <Button handleClick={addRValue}>4</Button>
-//           <Button handleClick={addRValue}>5</Button>
-//           <Button handleClick={addRValue}>6</Button>
-//           <Button handleClick={countR}>*</Button>
+//           <Button handleClick={addValue}>4</Button>
+//           <Button handleClick={addValue}>5</Button>
+//           <Button handleClick={addValue}>6</Button>
+//           <Button handleClick={count}>*</Button>
 //         </div>
 //         <div className={style.row}>
-//           <Button handleClick={addRValue}>1</Button>
-//           <Button handleClick={addRValue}>2</Button>
-//           <Button handleClick={addRValue}>3</Button>
-//           <Button handleClick={countR}>+</Button>
+//           <Button handleClick={addValue}>1</Button>
+//           <Button handleClick={addValue}>2</Button>
+//           <Button handleClick={addValue}>3</Button>
+//           <Button handleClick={count}>+</Button>
 //         </div>
 //         <div className={style.row}>
-//           <Button handleClick={addDotR}>.</Button>
-//           <Button handleClick={addRValue}>0</Button>
-//           <Button handleClick={evaluateR}>=</Button>
-//           <Button handleClick={countR}>-</Button>
+//           <Button handleClick={addDot}>.</Button>
+//           <Button handleClick={addValue}>0</Button>
+//           <Button handleClick={evaluate}>=</Button>
+//           <Button handleClick={count}>-</Button>
 //         </div>
 //       </div>
 //     </div>
 //   );
+const CalcComponent = ({inputValue,setIsLoading ,addRValue, clearRInput, countR, evaluateR, addDotR }) => {
+  return (
+    <div className={style.calcContainer}>
+            <Link to="/Second">Results</Link>
+            <button onClick={() => setIsLoading()}>set calc is loading</button>
+            <Link to="/Chat">Chat</Link>
+
+      <div className={style.row}>
+        <Result inputValue={inputValue} />
+        <ClearButton handleClear={clearRInput}>Clear</ClearButton>
+      </div>
+      <div className={style.numberContainer}>
+        <div className={style.row}>
+
+          <Button handleClick={addRValue}>7</Button>
+          <Button handleClick={addRValue}>8</Button>
+          <Button handleClick={addRValue}>9</Button>
+          <Button handleClick={countR}>/</Button>
+        </div>
+        <div className={style.row}>
+          <Button handleClick={addRValue}>4</Button>
+          <Button handleClick={addRValue}>5</Button>
+          <Button handleClick={addRValue}>6</Button>
+          <Button handleClick={countR}>*</Button>
+        </div>
+        <div className={style.row}>
+          <Button handleClick={addRValue}>1</Button>
+          <Button handleClick={addRValue}>2</Button>
+          <Button handleClick={addRValue}>3</Button>
+          <Button handleClick={countR}>+</Button>
+        </div>
+        <div className={style.row}>
+          <Button handleClick={addDotR}>.</Button>
+          <Button handleClick={addRValue}>0</Button>
+          <Button handleClick={evaluateR}>=</Button>
+          <Button handleClick={countR}>-</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 const mapStateToProps = (state) => ({
   isLoading: state.calcReducer.isLoading,
   results: state.calcReducer.results,
   //4
-  // inputValue: state.calcReducer.inputValue
+  inputValue: state.calcReducer.inputValue
 })
 
 const mapDispatchToProps = (dispatch) => ({
   setIsLoading: () => dispatch(setIsLoading()),
   //5
-  addResult: (result) => dispatch(addResult(result)),
-  // addRValue: (val) => dispatch(addRValue(val)),
-  // clearRInput: () => dispatch(clearRInput()),
-  // countR:(val)=>dispatch(countR(val)),
-  // evaluateR:()=> dispatch(evaluateR()),
-  // addDotR:()=> dispatch(addDotR())
+  // addResult: (result) => dispatch(addResult(result)),
+  addRValue: (val) => dispatch(addRValue(val)),
+  clearRInput: () => dispatch(clearRInput()),
+  countR:(val)=>dispatch(countR(val)),
+  evaluateR:()=> dispatch(evaluateR()),
+  addDotR:()=> dispatch(addDotR())
 
 })
 
