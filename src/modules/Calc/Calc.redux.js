688c0b86edf2dde9e63720c463ec801a22e45c42
Kurkiewicz <kurkiewd@emea.roche.com>
2021-08-12 13:36:29 +0200

    redux-sauce

--- src/modules/Calc/Calc.redux.js
+++ src/modules/Calc/Calc.redux.js
@@ -1,144 +1,142 @@
+import { createReducer } from 'reduxsauce'
+export const INITIAL_STATE = { isLoading: false, results: [], inputValue: '' }
 export const CALC_SET_IS_LOADING = 'SET CALC IS LOADING'
-
-export const CALC_ADD_RESULT = 'ADD CALC RESULT'
 export const CALC_REMOVE_RESULT = 'REMOVE CALC RESULT'
-//1
+export const CALC_ADD_RESULT = 'ADD CALC RESULT'
 export const CALC_ADD_VALUE = 'ADD CALC VALUE'
 export const CALC_CLEAR = 'CLEAR INPUT'
 export const CALC_COUNT = 'COUNT'
 export const CALC_EVAL = 'EVAL'
 export const CALC_DOT = 'DOT'
 
-export const setIsLoading = () => ({
+export const HANDLERS = {
+    [CALC_SET_IS_LOADING]: (state = INITIAL_STATE, { isLoading }) =>
+        ({ ...state, isLoading: !isLoading }),
+    [CALC_REMOVE_RESULT]: (state = INITIAL_STATE, { results }) => ({
+        ...state, results
+    }),
+    [CALC_ADD_RESULT]: (state = INITIAL_STATE, { results }) => ({
+        ...state, results
+    }),
+    [CALC_ADD_VALUE]: (state = INITIAL_STATE, { inputValue }) => ({
+        ...state, inputValue
+    }),
+    [CALC_CLEAR]: (state = INITIAL_STATE, { inputValue }) => ({
+        ...state, inputValue
+    }),
+    [CALC_COUNT]: (state = INITIAL_STATE, { inputValue }) => ({
+        ...state, inputValue
+    }),
+    [CALC_EVAL]: (state = INITIAL_STATE, { results }) => ({
+        ...state, results
+    }),
+    [CALC_DOT]: (state = INITIAL_STATE, { inputValue }) => ({
+        ...state, inputValue
+    }),
+};
+
+
+
+export const setIsLoading = (isLoading) => ({
     type: CALC_SET_IS_LOADING,
+    value: isLoading
 })
 
-export const addResult = (val) => ({
-    type: CALC_ADD_RESULT,
-    value: val
-})
-export const removeResult = (val) => ({
-    type: CALC_REMOVE_RESULT,
-    value: val
-})
-//2
-export const addRValue = (val) => ({
-    type: CALC_ADD_VALUE,
-    value: val
-})
-export const clearRInput = () => ({
-    type: CALC_CLEAR
-})
-export const countR = (val) => ({
-    type: CALC_COUNT,
-    value: val
-})
-export const evaluateR = () => ({
-    type: CALC_EVAL
-})
-export const addDotR = () => ({
-    type: CALC_DOT
-})
-
-export const initialState = {
-    isLoading: false,
-    results: [],
-    //3
-    inputValue: ''
+export const addResult = (val) => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    dispatch({
+        type: CALC_ADD_RESULT,
+        results: [...store.results, val]
+    })
 }
-//4
+export const removeResult = (val) => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    console.log(store)
+    for (let i = 0; i < store.results.length; i++) {
+        if (store.results[i] === val) {
+            store.results.splice(i, 1);
+        }
+    }
+    dispatch({
+        type: CALC_REMOVE_RESULT,
+        results: [...store.results]
+    })
+}
+
+export const addRValue = (val) => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    console.log(store)
+    if (store.inputValue === "0" || (val === "0" && store.inputValue === "")) {
+        store.inputValue = "0."
+    } else {
+        store.inputValue = store.inputValue + val;
+    }
+    dispatch({
+        type: CALC_ADD_VALUE,
+        inputValue: store.inputValue
+    })
+}
+export const clearRInput = () => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    store.inputValue = ''
+    dispatch({
+        type: CALC_CLEAR,
+        inputValue: store.inputValue
+
+    })
+}
+
+export const countR = (val) => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    if (store.inputValue.toString() !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
+        store.inputValue = store.inputValue + val
+    } else if (store.inputValue !== "") {
+        store.inputValue = store.inputValue.toString().slice(0, -1) + val
+    }
+    dispatch({
+        type: CALC_COUNT,
+        inputValue: store.inputValue
+    })
+}
+
 const regex = new RegExp(/[\W_]/);
 
-export const calcReducer = (store = initialState, action) => {
-    switch (action.type) {
-        case CALC_SET_IS_LOADING:
-            return {
-                ...store,
-                isLoading: !store.isLoading
-            }
-        case CALC_ADD_RESULT:
-            console.log(action)
-            return {
-                ...store,
-                results: [...store.results, action.value]
-            }
-        case CALC_REMOVE_RESULT:
-            console.log(store.results.length)
-            console.log(action)
-            for (let i = 0; i < store.results.length; i++) {
-                if (store.results[i] === action.value) {
-                    store.results.splice(i, 1);
-                }
-            }
-            return {
-                ...store,
-                results: [...store.results]
-
-            }
-            //5
-        case CALC_ADD_VALUE:
-            if (store.inputValue === "0" || (action.value === "0" && store.inputValue === "")) {
-                store.inputValue = "0."
-            } else {
-                store.inputValue = store.inputValue + action.value;
-            }
-            console.log(action)
-            return {
-                ...store,
-                inputValue: store.inputValue
-            }
-
-        case CALC_CLEAR:
-            console.log(store)
-            store.inputValue = ''
-            return {
-                ...store,
-                inputValue: store.inputValue
-            }
-        case CALC_COUNT:
-            console.log(action.value)
-            if (store.inputValue.toString() !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
-                store.inputValue = store.inputValue + action.value
-            } else if (store.inputValue !== "") {
-                store.inputValue = store.inputValue.toString().slice(0, -1) + action.value
-            }
-            return {
-                ...store,
-                inputValue: store.inputValue
-            }
-        case CALC_EVAL:
-            if (store.inputValue !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
-                // eslint-disable-next-line
-                const result = eval(store.inputValue)
-                store.results = [...store.results, result]
-                console.log(result)
-                store.inputValue = result
-            } else if (store.inputValue !== "") {
-                // eslint-disable-next-line
-                const result = eval(store.inputValue.toString().slice(0, -1))
-                store.results = [...store.results, result]
-                store.inputValue = result
-            }
-            return {
-                ...store,
-                results: store.results,
-                inputValue: store.inputValue
-            }
-        case CALC_DOT:
-            if (store.inputValue === "" || (store.inputValue === "0" && store.inputValue.toString().slice(-1).match(regex) == null)) {
-                store.inputValue = "0."
-            } else if (store.inputValue.toString().slice(-1).match(regex)) {
-                // eslint-disable-next-line
-                store.inputValue = store.inputValue
-            }
-            else {
-                store.inputValue = store.inputValue + "."
-            }
-            return {
-                ...store,
-                inputValue: store.inputValue
-            }
-        default:
-            return store;
+export const evaluateR = () => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    if (store.inputValue !== "" && store.inputValue.toString().slice(-1).match(regex) == null) {
+        // eslint-disable-next-line
+        const result = eval(store.inputValue)
+        store.results = [...store.results, result]
+        console.log(result)
+        store.inputValue = result
+    } else if (store.inputValue !== "") {
+        // eslint-disable-next-line
+        const result = eval(store.inputValue.toString().slice(0, -1))
+        store.results = [...store.results, result]
+        store.inputValue = result
     }
+    dispatch({
+        type: CALC_EVAL,
+        results: store.results,
+        inputValue: store.inputValue
+    })
 }
+
+export const addDotR = () => (dispatch, getState) => {
+    const store = getState().calcReducer;
+    if (store.inputValue === "" || (store.inputValue === "0" && store.inputValue.toString().slice(-1).match(regex) == null)) {
+        store.inputValue = "0."
+    } else if (store.inputValue.toString().slice(-1).match(regex)) {
+        // eslint-disable-next-line
+        store.inputValue = store.inputValue
+    }
+    else {
+        store.inputValue = store.inputValue + "."
+    }
+    dispatch({
+        type: CALC_DOT,
+        inputValue: store.inputValue
+
+    })
+}
+export default createReducer(INITIAL_STATE, HANDLERS)
--- src/modules/Main/Main.redux.js
+++ src/modules/Main/Main.redux.js
@@ -1,28 +1,30 @@
 import { chatReducer } from '../Chat/Chat.redux';
-import { calcReducer } from '../Calc/Calc.redux';
+import calcReducer from '../Calc/Calc.redux';
+import thunk from 'redux-thunk';
 
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 export const getStore = () => {
-  const reducers = combineReducers({
-    chatReducer,
-    calcReducer
-  })
+    const reducers = combineReducers({
+        chatReducer,
+        calcReducer
+    })
 
-  const composeEnhancers =
-    typeof window === 'object' &&
-      process.env.NODE_ENV !== 'production' &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) :
-      compose
+    const composeEnhancers =
+        typeof window === 'object' &&
+        process.env.NODE_ENV !== 'production' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) :
+        compose
 
-  const middlewares = [
-    // routerMiddleware(history),
-    // saveChangesMiddleware
-  ]
 
-  return createStore(
-    reducers,
-    composeEnhancers(applyMiddleware(...middlewares))
-  )
+    const middlewares = [
+        thunk,
+    ]
+
+
+    return createStore(
+        reducers,
+        composeEnhancers(applyMiddleware(...middlewares))
+    )
 }
